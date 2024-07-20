0ca69e0ec09b2bc162f9a0361934de3088d96699
hrantm <hrant.mirijanyan@gmail.com>
2024-07-20 05:46:28 -0700

    add reset and metris endpoints

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+	// mu             sync.Mutex
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const pathRoot = "."
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(pathRoot))))
-	mux.HandleFunc("/healthz", handleHealthEndpoint)
+
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(pathRoot)))
+	apiCfg := &apiConfig{}
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(appHandler))
+
+	mux.HandleFunc("/healthz", handleHealthz)
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
+
 	server := &http.Server{Handler: mux, Addr: ":" + port}
 	log.Printf("Serving on port: %s\n", port)
 	err := server.ListenAndServe()
@@ -19,8 +39,21 @@ func main() {
 	}
 }
 
-func handleHealthEndpoint(w http.ResponseWriter, r *http.Request) {
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset"))
 }
