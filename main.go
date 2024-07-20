3b083559f982661bd8c82cc9f2c4cd48d5185075
hrantm <hrant.mirijanyan@gmail.com>
2024-07-20 03:38:20 -0700

    add health endpoint

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ func main() {
 	const pathRoot = "."
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(pathRoot)))
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(pathRoot))))
+	mux.HandleFunc("/healthz", handleHealthEndpoint)
 	server := &http.Server{Handler: mux, Addr: ":" + port}
 	log.Printf("Serving on port: %s\n", port)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handleHealthEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
