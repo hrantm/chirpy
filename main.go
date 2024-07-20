bd09cc32eff1d25cde9195c0560fa02a30cc2628
hrantm <hrant.mirijanyan@gmail.com>
2024-07-20 02:23:44 -0700

    serve file for root path

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	port := "8080"
+	const pathRoot = "."
+	const port = "8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(pathRoot)))
 	server := http.Server{Handler: mux, Addr: ":" + port}
 	log.Printf("Serving on port: %s\n", port)
 	err := server.ListenAndServe()
